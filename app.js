fd054777e6df4ca8c24560495314b23ad284fadf
XenZi <58092775+XenZi@users.noreply.github.com>
2021-01-27 00:02:31 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,136 @@
+class drumKit {
+    constructor() {
+        this.pads = document.querySelectorAll(".pad");
+        this.playButton = document.querySelector(".play");
+        this.kickAudio = document.querySelector(".kick-sound");
+        this.snareAudio = document.querySelector(".snare-sound");
+        this.hihatAudio = document.querySelector(".hihat-sound");
+        this.index = 0;
+        this.bpm = 150;
+        this.isPlaying = null;
+        this.selects = document.querySelectorAll("select");
+        this.muteBtns = document.querySelectorAll(".mute");
+    }
+    activePad() {
+        this.classList.toggle("active");
+    }
+    repeat() {
+        let step = this.index % 8; // Whenever index comes to the 8 and divides by the module of eight, it becomes zero again so it loops over and over! Useful trick for a lot of things!.
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        // Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = "playTrack 0.3s alternate ease-in-out 2";
+            if(bar.classList.contains("active")) {
+                if(bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
+        this.index++;
+    }
+    start() {
+        console.log(this);
+        const interval = (60/this.bpm) * 1000;
+        if(!this.isPlaying) {
+            this.isPlaying = setInterval(() => { // Koristimo arrow function zato sto ne zelimo da nam this uzme prototipove od window objecta, vec samo od objekta koji je deo klase drumKit!
+                this.repeat();
+            }, interval);
+        }
+        else {
+            // Remove the interval
+            clearInterval(this.isPlaying);
+            this.isPlaying = null;
+            this.index = 0;
+        }        
+    }
+    updateBtn() {
+        if(!this.isPlaying) {
+            this.playButton.innerText = "Play";
+            this.playButton.classList.add("active");
+        }
+        else {
+            this.playButton.innerText = "Stop";
+            this.playButton.classList.remove("active");
+        }
+    }
+    changeSound(e) {
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+        switch(selectionName) {
+            case "kick-select":
+                this.kickAudio.src = selectionValue;
+                break;
+            case "snare-select":
+                this.snareAudio.src = selectionValue;
+                break;
+            case "hihat-select":
+                this.hihatAudio.src = selectionValue;
+                break;
+
+        }
+    }
+    mute(e) {
+        console.log(e.target);
+        const muteIndex = e.target.getAttribute("data-track");
+        e.target.classList.toggle("active");
+        if(e.target.classList.contains("active")) {
+            switch(muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 0;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 0;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 0;
+                    break;
+            }
+        }
+        else {
+            switch(muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 1;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 1;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 1;
+                    break;
+            }
+        }
+    }
+}
+
+const DrumKit = new drumKit();
+DrumKit.playButton.addEventListener("click", () => {
+    DrumKit.start();
+    DrumKit.updateBtn();
+});
+
+DrumKit.pads.forEach(pad => {
+    pad.addEventListener("click", DrumKit.activePad);
+    pad.addEventListener("animationend", function() {
+        this.style.animation = "";
+    });
+});
+DrumKit.selects.forEach(select => {
+    select.addEventListener("change", function(e) {
+        DrumKit.changeSound(e);
+    });
+});
+
+DrumKit.muteBtns.forEach((button) => {
+    button.addEventListener("click", function(e) {
+        DrumKit.mute(e);
+    });
+});
